664dc1730ae1980a9a9dd0b9d8b8c5c94cf7561b
Jake Fulton <146018017+jakefulton1@users.noreply.github.com>
2025-11-16 17:30:22 -0500

    Add Users.java Model in backend

---
+++ backend/rest/src/main/java/com/locasian/app/rest/Models/Users.java
@@ -0,0 +1,33 @@
+package com.locasian.app.rest.Models;
+import java.time.LocalDateTime;
+
+public class Users {
+    private int user_id;
+    private String username;
+    private LocalDateTime created_at;
+
+
+    public int get_user_id() {
+        return user_id;
+    }
+
+    public void set_user_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String get_username() {
+        return username;
+    }
+
+    public void set_username(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime get_created_at() {
+        return created_at;
+    }
+
+    public void set_created_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    } 
+}
