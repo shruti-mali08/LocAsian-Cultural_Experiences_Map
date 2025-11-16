c0ae78caf6e629a8fb82110976827ce509f46d3e
Jake Fulton <146018017+jakefulton1@users.noreply.github.com>
2025-11-16 17:36:10 -0500

    next step in backend

--- backend/rest/src/main/java/com/locasian/app/rest/Models/Users.java
+++ backend/rest/src/main/java/com/locasian/app/rest/Models/Users.java
@@ -7,27 +7,27 @@ public class Users {
     private LocalDateTime created_at;
 
 
-    public int get_user_id() {
-        return user_id;
-    }
+public int get_user_id() {
+    return user_id;
+}
 
-    public void set_user_id(int user_id) {
-        this.user_id = user_id;
-    }
+public void set_user_id(int user_id) {
+    this.user_id = user_id;
+}
 
-    public String get_username() {
-        return username;
-    }
+public String get_username() {
+    return username;
+}
 
-    public void set_username(String username) {
-        this.username = username;
-    }
+public void set_username(String username) {
+    this.username = username;
+}
 
-    public LocalDateTime get_created_at() {
-        return created_at;
-    }
+public LocalDateTime get_created_at() {
+    return created_at;
+}
 
-    public void set_created_at(LocalDateTime created_at) {
-        this.created_at = created_at;
-    } 
+public void set_created_at(LocalDateTime created_at) {
+    this.created_at = created_at;
+} 
 }
