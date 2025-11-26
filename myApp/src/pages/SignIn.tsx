b5df743034a469db759ab849c4e14778ecf0c101
Kwhites302 <kwhites302@gmail.com>
2025-11-26 10:23:15 -0500

    added button to bring to sign up page and fields for sign up

--- myApp/src/pages/SignIn.tsx
+++ myApp/src/pages/SignIn.tsx
@@ -1,25 +1,83 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton} from '@ionic/react';
+import { useHistory } from 'react-router';
+import { useState } from 'react';
 import AppLogo from '../assets/icons/AppLogo.svg';
+import './SignIn.css';
 
 const SignIn: React.FC = () => {
+  const history = useHistory();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSignIn = () => {
+    //sign in logic fill in later
+    setErrorMessage('Invalid username or password');
+
+    //if (authSuccess) {navigate to home screen}
+    //else {set error message}
+  }
+
+
   return (
     <IonPage>
-      <IonContent className="ion-padding" style={{ '--background': '#C5472A' }}>
-        <img src={AppLogo} alt="Logo" style={{ width: '150px', margin: '20px auto', display: 'block' }} />
+      <IonContent className="ion-padding reggae-font" style={{ '--background': '#C5472A' }}>
+        {/* Display the app logo at the top */}
+        <img src={AppLogo} alt="Logo" style={{ width: '258px', margin: '20px auto', display: 'block' }} />
 
-        <IonItem>
-          <IonLabel position="floating">Username</IonLabel>
-          <IonInput type="text" placeholder="Enter username"></IonInput>
+        <div style = {{ marginTop: '20px'}}>
+          <IonLabel className="title-font"> Sign In</IonLabel> 
+        </div>
+        <br></br>
+
+        {/* Username field */}
+        <IonLabel position="floating" >Username</IonLabel>
+        <IonItem style = {{ marginTop: '10px' }}>
+          <IonInput 
+              type="text"
+              fill = "solid"
+              labelPlacement='stacked'
+              clearInput = {true}
+              placeholder="Enter username"
+              ></IonInput>
         </IonItem>
 
-        <IonItem>
-          <IonLabel position="floating">Password</IonLabel>
-          <IonInput type="password" placeholder="Enter password"></IonInput>
+        <br></br>
+
+        {/* Password field */}
+          <IonLabel position="floating" >Password</IonLabel>
+        <IonItem style = {{ marginTop: '10px' }}>
+          <IonInput 
+            type="password" 
+            fill = "solid"
+            placeholder="Enter password"
+            clearInput = {true}
+          ></IonInput>
         </IonItem>
 
-        // random ah button
-        <IonButton>Default</IonButton>
-        <IonButton disabled={true}>Disabled</IonButton>
+        {/* Log in button **add logic** */} 
+        <IonButton 
+          expand = "block" 
+          style = {{marginTop: '50px'}} 
+          shape = "round" 
+          fill = "outline" 
+          color = 'light'
+          onClick={() => history.push('/Home')}
+        >Log In</IonButton>
+
+        {/* Sign up button */}
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <span style={{ color: '#000' }}>Don't have an account? </span>
+          <span 
+            style={{ 
+              color: '#007bff', 
+              cursor: 'pointer', 
+              fontWeight: 'bold',
+              textDecoration: 'underline'
+            }}
+            onClick={() => history.push('/SignUp')}
+          >
+            Sign Up
+          </span>
+        </div>
       </IonContent>
     </IonPage>
   );
--- myApp/src/pages/SignUp.tsx
+++ myApp/src/pages/SignUp.tsx
@@ -1,16 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage, IonButton, IonLabel, IonItem, IonInput} from '@ionic/react';
+import './SignIn.css';
+import {useState} from 'react';
+import AppLogo from '../assets/icons/AppLogo.svg';
 
 const SignUp: React.FC = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSignUp = () => {
+    //sign up logic fill in later
+    
+  }
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Sign Up</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <h1>Sign Up Page</h1>
-        <p>Sign up form will go here</p>
+      <IonContent className="ion-padding reggae-font" style={{ '--background': '#C5472A' }}>
+        
+        <img src={AppLogo} alt="Logo" style={{ width: '141px', margin: '20px auto', display: 'block' }} />
+
+        <div style = {{ marginTop: '40px'}}>
+          <h1>Sign Up</h1>
+        </div>
+
+        {/* Username field */}
+        <IonLabel position="floating" >Enter valid username (9-12 characters):</IonLabel>
+        <IonItem style = {{ marginTop: '10px' }}>
+          <IonInput 
+              type="text"
+              fill = "solid"
+              labelPlacement='stacked'
+              clearInput = {true}
+              placeholder="Enter username"
+              ></IonInput>
+        </IonItem>
+
+        <br></br>
+
+        {/* Password field */}
+          <IonLabel position="floating" >Enter password (at least one special character):</IonLabel>
+        <IonItem style = {{ marginTop: '10px' }}>
+          <IonInput 
+            type="password" 
+            fill = "solid"
+            placeholder="Enter password"
+            clearInput = {true}
+          ></IonInput>
+        </IonItem>
+
+        {/* Sign up button */}
+        <IonButton 
+          expand = "block" 
+          style = {{marginTop: '50px'}} 
+          shape = "round" 
+          fill = "outline" 
+          color = 'light'
+          onClick=  {handleSignUp}
+        >Log In</IonButton>
+
       </IonContent>
     </IonPage>
   );
