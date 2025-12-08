ca0afed55f7206d6adf21233ca8c6c74b3451d79
Kwhites302 <kwhites302@gmail.com>
2025-12-08 18:48:54 -0500

    Remove duplicate lowercase config folder from Git

--- backend/rest/src/main/java/com/locasian/app/rest/Config/CorsConfig.java
+++ backend/rest/src/main/java/com/locasian/app/rest/Config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.locasian.app.rest.config;
+package com.locasian.app.rest.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
