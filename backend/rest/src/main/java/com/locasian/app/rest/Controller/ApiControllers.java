3e5da58df365c5c151d01a97921e935107285ed6
Shruti Mali <shrutijmali08@gmail.com>
2025-12-08 23:15:02 -0500

    Commented an unused import in ApiControllers.java

--- backend/rest/src/main/java/com/locasian/app/rest/Controller/ApiControllers.java
+++ backend/rest/src/main/java/com/locasian/app/rest/Controller/ApiControllers.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
